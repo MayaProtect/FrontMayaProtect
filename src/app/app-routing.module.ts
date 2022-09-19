f2b20b62be3a23b31a14e3f9eb21757c06347b2b
Souhaïb Massrour <massroursouhaib@hotmail.com>
2022-09-19 18:34:27 +0200

    chore: modification change angular material to primeng update table component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./pages/home/home.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AddEventComponent } from "./pages/add-event/add-event.component";
 
 const routes: Routes = [
-  { path: 'home-component', component: HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'event', component: AddEventComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/features/table/table.component.ts
+++ src/app/features/table/table.component.ts
@@ -1,23 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
+import {LazyLoadEvent} from "primeng/api";
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
-  public data_display : any;
+export class TableComponent implements OnInit{
+  public data_display : any = [];
   public data_header: any = [];
-  @Input('data_table') my_data: Array<any> = [];
+  public total_page: number = 0;
+  public loadingBody: Boolean = true;
+  @Input() data_table: any = null;
+  @Input('count_hives') count_hives: number = 0;
+  @Input('elements_per_page') elements_per_page: number = 0;
+  @Output() newNumberPage = new EventEmitter<any>();
 
-  constructor() {
-  }
-  ngOnInit(): void {
-    // fonction pour extraire les key du tableau
-    Object.keys(this.my_data[0]).forEach(key => {
+  async ngOnInit() {
+    this.total_page = Math.ceil(this.count_hives / this.elements_per_page);
+    Object.keys(this.data_table[0]).forEach(key => {
       this.data_header.push(key)
     })
-    this.data_display = this.my_data;
+    this.data_display = this.data_table;
+  }
+
+  constructor() {
+
+  }
+  ngOnChanges() {
+    this.data_display = this.data_table;
   }
   displayedColumns: string[] = this.data_header;
+
+  loadHives (event: LazyLoadEvent) {
+    this.newNumberPage.emit(event)
+  }
 }
